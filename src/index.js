4326c995047900b9bc0b694c6c41c2523a26ccdf
StevenPss <pulessteven@gmail.com>
2021-10-02 00:53:50 +0200

    added animations

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import * as bootstrap from "bootstrap";
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import helloWorld from "./scripts/helloWorld";
-import navbar from "./scripts/navbar";
 import loadDataTables from "./scripts/loadDataTables";
+
+import animations from "./scripts/animations";
 
 // expose bootstrap to the window
 window.bootstrap = bootstrap;
 
 helloWorld();
-navbar();
 loadDataTables();
+
+animations();
 
